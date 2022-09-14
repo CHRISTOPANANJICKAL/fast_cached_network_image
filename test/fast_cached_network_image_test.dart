59bab76b3c19412682fb2fe0e712c86c4d6e6909
christo pb <christo@ateamsoftsolutions.com>
2022-09-15 02:43:30 +0530

    added test code

---
+++ test/fast_cached_network_image_test.dart
@@ -0,0 +1,15 @@
+import 'package:fast_cached_network_image/src/fast_cached_image.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  String storageLocation = 'E:/fast';
+  String url1 = 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg';
+
+  test('Initialize fast cached image configurations', () async {
+    await FastCachedImageConfig.init(path: storageLocation, clearCacheAfter: const Duration(days: 15));
+  }, timeout: const Timeout(Duration(seconds: 2)));
+
+  testWidgets('FastCachedImage can be displayed', (WidgetTester tester) async {
+    await tester.pumpWidget(FastCachedImage(url: url1));
+  });
+}
